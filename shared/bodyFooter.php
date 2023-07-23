9faeda0f9e62beeb51bebea699f1d77715efd711
Mariya Lazarova <67010799+MrsMariyaLazarova@users.noreply.github.com>
2023-07-23 18:27:37 +0300

    Create bodyFooter.php

---
+++ shared/bodyFooter.php
@@ -0,0 +1,36 @@
+<img src="images/grass.png" width="1000" alt="" id="grass">
+
+<footer class="section footer-classic context-dark bg-image bg-light">
+  <div class="container">
+    <div class="row row-30">
+      <div class="col-md-6 col-xl-6">
+        <div class="pr-xl-6">
+          <p>Аз съм Светлана Тодорова Лазарова, старши учител, IПКС. Част съм от педагогическия екип на ДГ№2 „Знаме на Мира” – гр. Балчик.</p>
+          <!-- Rights-->
+          <p class="rights"><span>Copyright </span><span>© </span><span class="copyright-year">2020</span><span></span><span> . </span><span>All Rights Reserved.</span></p>
+        </div>
+      </div>
+      <div class="col-md-6" style="padding: 22px;">
+        <h5 style="text-decoration: underline;"><b>Контакти</b></h5>
+        <dl class="contact-list">
+          <dt>Адрес:</dt>
+          <dd>ДГ №2 „Знаме на Мира“, гр. Балчик, ул. „Арда“№3</dd>
+        </dl>
+        <dl class="contact-list">
+          <dt>email:</dt>
+          <dd><a href="mailto:#">svetluna1@gmail.com</a></dd>
+        </dl>
+        <dl class="contact-list">
+          <dt>телефон за връзка:</dt>
+          <dd><a href="tel:#">+359 887 248 066</a>
+          </dd>
+        </dl>
+      </div>
+    </div>
+  </div>
+  <div class="row no-gutters social-container">
+    <div class="col"><a class="social-inner" href="https://www.facebook.com/svetla.balchik"><span class="icon mdi mdi-facebook"></span><span>Facebook</span></a></div>
+    <div class="col"><a class="social-inner" href="https://www.instagram.com/svetla_balchik/?igshid=1a2k73g7tbgk4&fbclid=IwAR3fgXtMQJAY39SX5l2BQts8MspL65WlusEYPPizcxcfHGeQtnKDbkFwlfQ"><span class="icon mdi mdi-instagram"></span><span>instagram</span></a></div>
+  </div>
+
+</footer>
